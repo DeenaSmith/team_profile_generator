86261a0c58d7d55bf617be64bcd2a2161650ec70
Deena Smith <deenasmith1630@gmail.com>
2022-04-15 13:10:12 -0500

    initial prompt works, all questions added, needs to prompt additional questions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 // Requirements
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require("fs");
+
 
 
 // Library JS
@@ -12,36 +13,143 @@ const Manager = require("./lib/Manager");
 const newStaffInfo = [];
 
 
-// Questions for prompt
-const questions = async () => {
-    const answers = await inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your name?",
-                name: "name",
-            },
-            {
-                type: "input",
-                message: "What is your employee ID?",
-                name: "id",
-            },
-            {
-                type: "input",
-                message: "What is your email address?",
-                name: "email",
-            },
-            {
-                type: "list",
-                message: "What is your office number?",
-                name: "number",
-            }
-        ]).then(answers => {
-            console.log(answers);
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.number);
-            teamMembers.push(manager);
+// Questions for  initial prompt
+const managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your employee ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your office number?",
+            name: "number"
+        }
+    ]).then(answers => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.number);
+        newStaffInfo.push(manager);
 
-            promptMenu()
-        })
-        
+        promptMenu()
+    })
+
 };
+
+
+// Options to add employees
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "How would you like to proceed?",
+            choices: ["Add engineer", "Add intern", "No further entries"]
+        }
+    ])
+        .then(userChoice => {
+            switch (userChoice.menu) {
+                case "add engineer":
+                    engineerQuestions();
+                    break;
+                case "add intern":
+                    internQuestions();
+                    break;
+                default:
+                    buildTeam();
+            }
+        })
+
+};
+
+
+
+// Add engineer
+const engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their Github username?"
+        }
+
+    ]).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+
+        newStaffInfo.push(engineer)
+        promptMenu();
+    })
+};
+
+
+
+// Add intern
+const internQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school did they attend?"
+        }
+
+    ]).then(answers => {
+        console.log(answers);
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+
+        newStaffInfo.push(intern)
+        promptMenu();
+    })
+};
+
+
+
+// Takes user input and builds team to file
+// const buildTeam = () => 
+
+
+
+
+
+
+managerQuestions();
